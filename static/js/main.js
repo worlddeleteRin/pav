0907bd1dc2eb575a269e0981995d8552e9d15a7c
Rin NoName <noname@MacBook-Pro-Rin.local>
2020-11-10 13:53:46 +0300

    new commit

--- static/js/main.js
+++ static/js/main.js
@@ -37,6 +37,38 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function() {
+    $('.product__page__container__image__toslide').slick({
+    //slide: 'div',
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    // mobileFirst: true,
+    dots: false,
+    infinite: true,
+    // cssEase: 'ease',
+    swipe: true,
+    arrows: true,
+    autoplay: true,
+    autoplayspeed: 1000,
+    // lazyLoad: 'ondemand',
+    prevArrow: $('#prev_1'),
+    nextArrow: $('#next_1'),
+})
+});
+
+$(document).ready(function() {
+$(".header__main__wrapper").addClass("sub")
+
+$(".call__button__1 span").click(function() {
+showModal();
+});
+
+});
+
+
+
+
+
 
 
 
