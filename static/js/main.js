d277c011320bc2cfe9fe33667284873c8d62ecf8
Rin NoName <noname@MacBook-Pro-Rin.local>
2020-11-10 14:14:27 +0300

    new commit

--- static/js/main.js
+++ static/js/main.js
@@ -38,22 +38,27 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-    $('.product__page__container__image__toslide').slick({
-    //slide: 'div',
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    // mobileFirst: true,
-    dots: false,
-    infinite: true,
-    // cssEase: 'ease',
-    swipe: true,
-    arrows: true,
-    autoplay: true,
-    autoplayspeed: 1000,
-    // lazyLoad: 'ondemand',
-    prevArrow: $('#prev_1'),
-    nextArrow: $('#next_1'),
-})
+//     $('.product__page__container__image__toslide').slick({
+//     //slide: 'div',
+//     slidesToScroll: 1,
+//     slidesToShow: 1,
+//     // mobileFirst: true,
+//     dots: false,
+//     infinite: true,
+//     // cssEase: 'ease',
+//     swipe: true,
+//     arrows: true,
+//     autoplay: true,
+//     autoplayspeed: 1000,
+//     // lazyLoad: 'ondemand',
+//     prevArrow: $('#prev_1'),
+//     nextArrow: $('#next_1'),
+// })
+    $(".product__page__container__image__toslide").owlCarousel({
+        items: 1,
+        dots: true,
+        loop: true,
+    })
 });
 
 $(document).ready(function() {
