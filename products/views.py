d125a18cc8ee2b24a8198528e32b74ac88903560
Rin NoName <noname@MacBook-Pro-Rin.lan>
2021-04-22 10:46:23 +0300

    new commit

--- products/views.py
+++ products/views.py
@@ -8,7 +8,6 @@ from pav.settings import *
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-l
 from django.utils import translation
 
 
